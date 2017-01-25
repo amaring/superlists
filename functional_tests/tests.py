c05c9fcf7d304ab92c0a77af2f5bc30320df9730
Amanda Maring <amarin02@baker.edu>
2017-01-25 16:54:10 -0500

    added redirect into views and added info to functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Edith has heard about a cool new online to-to app.
         #She goes to check out its homepage.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.server_url)
 
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -38,9 +38,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
 
 
-        #When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
+        #When she hits enter, she is taken to a new URL,
+        #and now the page lists "1: Buy peacock feathers" as an item in a
+        #to-do list table
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.curent_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 
@@ -51,20 +54,36 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         #The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        
+        #Now a new user, Francis, comes along to the site
 
-        #Edith wonders whether the site will remember her list. Then she sees
-        #that the site has generated a unique URL for her -- there is some explanatory
-        #text to that effect.
-        self.fail('Finish the test!')
+        ##We us a new browser session to make sure no information
+        ##of Edith's is coming through from cookies, etc
+        self.browser(quit)
+        self.browser = webdriver.Firefox()
 
+        #Francis visists the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-#She visits that URL - her to-do list is stil there.
+        #Francis starts a new list by entering a new item. He is less
+        #interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
 
-#Satisfied, she goes back to sleep
+        #Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+        #Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        #Satisfied, they both go back to sleep
