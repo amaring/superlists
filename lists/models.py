08e6cc8e181edc006249b6bb8a8b4bc112ed9a73
Amanda Maring <amarin02@baker.edu>
2017-02-11 16:13:08 -0500

    End of chapter 12

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
-# Create your models here.
 class List(models.Model):
+
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
@@ -10,9 +10,9 @@ class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
 
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
     def __str__(self):
         return self.text
-
-class Meta:
-    ordering = ('id',)
-    unique_together = ('list', 'text')
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,12 +1,14 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
-from django.core.exceptions import ValidationError
+
 
 class ItemModelTest(TestCase):
 
     def test_default_text(self):
         item = Item()
         self.assertEqual(item.text, '')
+
 
     def test_item_is_related_to_list(self):
         list_ = List.objects.create()
@@ -15,12 +17,15 @@ class ItemModelTest(TestCase):
         item.save()
         self.assertIn(item, list_.item_set.all())
 
+
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
         item = Item(list=list_, text='')
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
+
+
 
     def test_duplicate_items_are_invalid(self):
         list_ = List.objects.create()
@@ -29,12 +34,14 @@ class ItemModelTest(TestCase):
             item = Item(list=list_, text='bla')
             item.full_clean()
 
+
     def test_CAN_save_same_item_to_different_lists(self):
         list1 = List.objects.create()
         list2 = List.objects.create()
         Item.objects.create(list=list1, text='bla')
         item = Item(list=list2, text='bla')
-        item.full_clean() #should not raise
+        item.full_clean()  # should not raise
+
 
     def test_list_ordering(self):
         list1 = List.objects.create()
@@ -42,13 +49,16 @@ class ItemModelTest(TestCase):
         item2 = Item.objects.create(list=list1, text='item 2')
         item3 = Item.objects.create(list=list1, text='3')
         self.assertEqual(
-            Item.objects.all(),
+            list(Item.objects.all()),
             [item1, item2, item3]
         )
+
 
     def test_string_representation(self):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
+
+
 
 class ListModelTest(TestCase):
 
